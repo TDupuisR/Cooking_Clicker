bf3a05ed0998f7613ad182ab2a26f6db4d0da275
BVerstaen <bverstaen04@gmail.com>
2023-09-27 15:59:24 +0200

    Fix Switch Screen
    Switch screen marche enfin ! J'ai arrêté d'être CON.

--- Cooking_Clicker/Assets/Script/SwitchScript.cs
+++ Cooking_Clicker/Assets/Script/SwitchScript.cs
@@ -1,55 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SwitchScript : MonoBehaviour
 {
     [Header("Screens")]
-    [SerializeField] private Transform ServiceGameObject;
-    [SerializeField] private Transform CookingGameObject;
-    [SerializeField] private Transform ProductionGameObject;
+    [SerializeField] private RectTransform ServiceGameObject;
+    [SerializeField] private RectTransform CookingGameObject;
+    [SerializeField] private RectTransform ProductionGameObject;
 
     [Space(5)]
     [SerializeField] private float m_transitionSpeed;
-
-    private void Start()
-    {
-        print(ProductionGameObject.localPosition);
-    }
+    bool IsBusy;
 
     /*
      PAS ENCORE FINI !
      */
 
-    public void StartSwitchScreen(int Screen) => StartCoroutine(SwitchScreen(Screen));
+    float i = 1;
+    //-360
+    //360
+    //720
+    public void StartSwitchScreen(int Screen) { if (!IsBusy) StartCoroutine(SwitchScreen(Screen)); }
 
     IEnumerator SwitchScreen(int Offset)
     {
+        IsBusy = true;
         float timeElapsed = 0;
 
         //Determine start and ending position of each Screens
-        Vector2 startServicePosition = ServiceGameObject.localPosition;
-        Vector2 endServicePosition = new Vector2(-1280 * Offset, 0f);
-        
-        Vector2 startCookingPosition = ServiceGameObject.localPosition;
-        Vector2 endCookingPosition = new Vector2(1280 - 1280 * Offset, 0f);
-        
-        Vector2 startProductionPosition = ServiceGameObject.localPosition;
-        Vector2 endProductionPosition = new Vector2(2560 - 1280 * Offset, 0f);
+        Vector3 startServicePosition = ServiceGameObject.position;
+        Vector3 endServicePosition = new Vector3(-720 * Offset + 360, 640f, 0f);
 
+        Vector3 startCookingPosition = CookingGameObject.position;
+        Vector3 endCookingPosition = new Vector3(-720 * Offset + 1080, 640f, 0f);
+
+        Vector3 startProductionPosition = ProductionGameObject.position;
+        Vector3 endProductionPosition = new Vector3(-720 * Offset + 1800, 640f, 0f);
+
+        print(endServicePosition);
 
         while (timeElapsed < m_transitionSpeed)
         {
-            ServiceGameObject.localPosition = Vector3.Lerp(startServicePosition, endServicePosition, timeElapsed / m_transitionSpeed);
-            CookingGameObject.localPosition = Vector3.Lerp(startCookingPosition, endCookingPosition, timeElapsed / m_transitionSpeed);
-            ProductionGameObject.localPosition = Vector3.Lerp(startProductionPosition, endProductionPosition, timeElapsed / m_transitionSpeed);
-            
+            ServiceGameObject.position = Vector3.Lerp(startServicePosition, endServicePosition, timeElapsed / m_transitionSpeed);
+            CookingGameObject.position = Vector3.Lerp(startCookingPosition, endCookingPosition, timeElapsed / m_transitionSpeed);
+            ProductionGameObject.position = Vector3.Lerp(startProductionPosition, endProductionPosition, timeElapsed / m_transitionSpeed);
+
             timeElapsed += Time.deltaTime;
             yield return null;
         }
 
-        ServiceGameObject.localPosition = endServicePosition;
-        CookingGameObject.localPosition = endCookingPosition;
-        ProductionGameObject.localPosition = endProductionPosition;
+        ServiceGameObject.position = endServicePosition;
+        CookingGameObject.position = endCookingPosition;
+        ProductionGameObject.position = endProductionPosition;
+
+        IsBusy = false;
     }
 }
